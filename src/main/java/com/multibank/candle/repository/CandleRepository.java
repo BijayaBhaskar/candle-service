25c7a0907c5b04736b28d50824c790a05efa53cb
Bijaya Bhaskar Swain <sbijayabhaskar@email.com>
2026-03-02 15:54:35 +0530

    feat(db): add atomic insert query using PostgreSQL ON CONFLICT

--- src/main/java/com/multibank/candle/repository/CandleRepository.java
+++ src/main/java/com/multibank/candle/repository/CandleRepository.java
@@ -6,12 +6,22 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
+/**
+ * Repository for CandleEntity persistence operations.
+ *
+ * @author Bijaya Bhaskar Swain
+ */
 @Repository
 public interface CandleRepository extends JpaRepository<CandleEntity, Long> {
 
+    /**
+     * Fetch historical candles ordered by bucket time ascending.
+     * Used by History API.
+     */
     List<CandleEntity> findBySymbolAndIntervalAndBucketTimeBetweenOrderByBucketTimeAsc(
             String symbol,
             Interval interval,
@@ -20,16 +30,24 @@ public interface CandleRepository extends JpaRepository<CandleEntity, Long> {
     );
 
 
+    /**
+     * Atomic upsert operation.
+     * Inserts a new candle if not exists,
+     * otherwise updates high/low/close/volume atomically.
+     * Concurrency-safe under high load.
+     */
     @Modifying
-    @Query(""" 
-        UPDATE CandleEntity c
-        SET c.high = CASE WHEN c.high < :price THEN :price ELSE c.high END,
-            c.low = CASE WHEN c.low > :price THEN :price ELSE c.low END,
-            c.close = :price,
-            c.volume = c.volume + 1
-        WHERE c.symbol = :symbol
-        AND c.interval = :interval
-        AND c.bucketTime = :bucket
-        """)
+    @Transactional
+    @Query(value = """
+        INSERT INTO candles ( symbol, interval, bucket_time, open, high, low, close, volume)
+        VALUES (:symbol, :interval, :bucket, :price, :price, :price, :price, 1 )
+        ON CONFLICT (symbol, interval, bucket_time)
+        DO UPDATE SET
+            high = GREATEST(candles.high, EXCLUDED.high),
+            low = LEAST(candles.low, EXCLUDED.low),
+            close = EXCLUDED.close,
+            volume = candles.volume + 1
+        """,
+            nativeQuery = true)
     int updateCandle(String symbol, Interval interval, Long bucket, double price);
 }
